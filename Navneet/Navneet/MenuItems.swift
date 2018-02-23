febd12c5941e51687be80cd44ce7688d4551f5d9
navunavneet <35268366+navunavneet@users.noreply.github.com>
2018-02-23 10:40:53 -0500

    Add files via upload

---
+++ Navneet/Navneet/MenuItems.swift
@@ -0,0 +1,32 @@
+//
+//  MenuItems.swift
+//  Navneet
+//
+//  Created by MacStudent on 2018-02-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+class MenuItems {
+    let names:[String] = [
+    
+    "mp","BBQ Chiken Pizza",
+    "pprroni Pizza","sausage pizza",
+    "seafood Pizza","sausage deep fish",
+    "meat lovers deep fish","veggie lover deep dish",
+    "BBQ chiken deep fish","mushroom deep dish"
+    ]
+    let prices:[Double] = [
+    7.95,11.49,
+    8.45,8.45,
+    12.75,10.65,
+    12.35,10.00,
+    16.60,11.25
+    ]
+    let special:[Bool] = [
+    false,true,
+    false,false,
+    false,false,
+    true,false,
+    false,true]
+}
